539577765ff7bbdc4d6759f53d258bd7bcb1c34b
Keyle777 <1059819521@qq.com>
2023-09-19 21:24:25 +0800

    第二次提交

--- steamGameCommunityFrontPageVue2/src/main.js
+++ steamGameCommunityFrontPageVue2/src/main.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import { createPinia, PiniaVuePlugin } from 'pinia'
+import Vue from 'vue';
+import { createPinia, PiniaVuePlugin } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-import './assets/main.css'
+app.config.productionTip = false;
 
-Vue.use(PiniaVuePlugin)
+import './assets/main.css';
+
+Vue.use(PiniaVuePlugin);
 
 new Vue({
-  router,
-  pinia: createPinia(),
-  render: (h) => h(App)
-}).$mount('#app')
+	router,
+	pinia: createPinia(),
+	render: (h) => h(App),
+}).$mount('#app');
