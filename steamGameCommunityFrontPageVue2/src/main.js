7c48b8e79d18f7d46ac873c502c9c4137034a070
Keyle777 <1059819521@qq.com>
2023-09-19 21:31:14 +0800

    node

--- steamGameCommunityFrontPageVue2/src/main.js
+++ steamGameCommunityFrontPageVue2/src/main.js
@@ -3,10 +3,7 @@ import { createPinia, PiniaVuePlugin } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
-import Antd from 'ant-design-vue';
-
 import './assets/main.css';
-Vue.use(Antd);
 
 Vue.use(PiniaVuePlugin);
 
